490609a2d6510d6ad13115140f74ed37f8fb6090
Nicholas Kinsey <pyro@feisty.io>
2013-02-17 12:51:20 +1000

    init

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+	
+	// scroll to top on mobile devices
+	if ($(".row").width() <= 320){
+    	$('html, body').animate({scrollTop:0}, 100);
+    }
+	
+});
+
+$(function(){
+    
+    
+    	
+});
